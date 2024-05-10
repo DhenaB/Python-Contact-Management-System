e6c001bf9c9931f6ea844316c90bfad53c3e65d3
Dhena Begum <115383590+DhenaB@users.noreply.github.com>
2024-05-10 17:39:38 -0400

    project2.py

--- project1.py
+++ project1.py
@@ -1,164 +1,108 @@
-import random
 
-# array of goodRespone and errorResponse
-goodResponse = ["Nice work!" , "Keep up the good work!", "Very good!",  "Excellent",]
-errorResponse = ["Wrong. Try once more.", "No. Please try again.", "Don't give up!", "No. Keep trying."]
+#function to display the menu
+def displayMenu():
+    
+    print("\nMenu")
+    print("-----------------------------------")
+    print("1.Look up an email address")   
+    print("2.Add a new name and email address")
+    print("3.Change an existing email address")
+    print("4.Delete a name and email address")
+    print("5.Quit the program")
 
-#addition function to add two numbers
-def addition():
-    correct = 0
-    wrong = 0
-    if (difficulty == "1"):
-        rangeValue = 9
-    if(difficulty=="2"):
-        rangeValue = 99
-    #get two numbers    
-    num1 = random.randint(0,rangeValue)
-    num2 = random.randint(0,rangeValue)
-    
-    print("\nHow much is ", num1, "+" , num2)
-    #ask user to enter answer
-    result = input("Enter your answer (-1 to exit): ")
-    
-    res = str(num1+num2)
-    
-    if(result == "-1"):
-        return -1
-    if(result == res):
-        correct+=1
-        print(goodResponse[random.randint(0,len(goodResponse)-1)])
+    #ask user to enter the option
+    option=int(input("\nEnter your choice: "))
+    return option
+
+#function for look up email address
+def lookUpEmail(emailDict, name):
+    if name in emailDict.keys():
+        return emailDict[name]
+
+#function to add name and email address    
+def addNameEmail(emailDict, name, email):
+    if name not in emailDict:
+        emailDict[name] = email
+        print("Name and address have been added")
     else:
-        wrong+=1
-        print(errorResponse[random.randint(0,len(errorResponse)-1)])
-    return 0
+        print("That name already exists\n")
 
-#substraction function to subtract two numbers
-def subtraction():
-    correct = 0
-    wrong = 0
-    if (difficulty == "1"):
-        rangeValue = 9
-    if(difficulty=="2"):
-        rangeValue = 99
-        
-    #get two numbers     
-    num1 = random.randint(0,rangeValue)
-    num2 = random.randint(0,rangeValue)
-    
-    if(num1<num2):
-        num1,num2 = num2,num1
-        
-    print("\nHow much is ", num1, "-" , num2)
-    #ask user to enter answer
-    result = input("Enter your answer (-1 to exit): ")
+#function for change an existing email
+def changeEmail(emailDict, name, newemail):
+    if name in emailDict.keys():
+        emailDict[name] = newemail
 
-    res = str(num1-num2)
-    
-    if(result == "-1"):
-        return -1
-    if(result == res):
-        correct+=1
-        print(goodResponse[random.randint(0,len(goodResponse)-1)])
+#funtion to delete name and email address
+def deleteNameEmail(emailDict, name):
+    if name in emailDict.keys():
+        emailDict[name]=None
+        del emailDict[name]
+        print("Information deleted")
     else:
-        wrong+=1
-        print(errorResponse[random.randint(0,len(errorResponse)-1)])
-    return 0
+        print("The specified name was not found\n")
 
-#function to multiply two numbers
-def multiplication():
-    correct = 0
-    wrong = 0
-    if (difficulty == "1"):
-        rangeValue = 9
-    if(difficulty=="2"):
-        rangeValue = 99
-        
-    #get two numbers     
-    num1 = random.randint(0,rangeValue)
-    num2 = random.randint(0,rangeValue)
-    
-    print("\nHow much is ", num1, "*" , num2)
-    #ask user to enter answer
-    result = input("Enter your answer (-1 to exit): ")
+#function for load and read information from the file
+def loadFromFile():
+    emailDict=dict()
+    try:
+        f = open('emails.txt', 'r') 
+        lines = f.readlines()
+        for i in range(0, len(lines), 2):
+            emailDict[lines[i].strip()] = lines[i+1].strip()
+        f.close()
+    except:
+        pass
+    return emailDict
 
-    res = str(num1*num2)
-    
-    if(result == "-1"):
-        return -1
-    if(result == res):
-        correct+=1
-        print(goodResponse[random.randint(0,len(goodResponse)-1)])
-    else:         
-        wrong+=1
-        print(errorResponse[random.randint(0,len(errorResponse)-1)])
-    return 0
+#function to save all information in a file
+def saveInFile(emailDict):   
+    f = open('emails.txt', 'w')
+    for key, value in emailDict.items():
+        f.write(key+"\n")
+        f.write(value+"\n")
+    f.close()
 
-#function to do division
-def division():
-    correct = 0
-    wrong = 0
-    if (difficulty == "1"):
-        rangeValue=9
-    if(difficulty=="2"):
-        rangeValue = 99
+#main function
+def main():
+    emailDict = loadFromFile()
+    while True:
+        choice = displayMenu()
+        if choice==1:
+            name=input("Enter a name: ")
+            try:
+                email=lookUpEmail(emailDict, name)
+                if(email!=None):
+                    print("Name:",name)
+                    print("Email:",email)
+                else:
+                    print("The specified name was not found")
+            except:
+                print("The specified name was not found")
+        elif choice==2:
+            name=input("Enter name: ")
+            email=input("Enter email Address: ")
+            addNameEmail(emailDict, name, email)
+            
+        elif choice==3:
+            name=input("Enter name: ")
+            email=input("Enter new address: ")
+            changeEmail(emailDict, name, email)
+            print("Information updated")
+        elif choice==4:
+            name=input("Enter the name: ")
+            try:
+                deleteNameEmail(emailDict, name)
+            except:
+                print("Something went wrong!!! please try again later")
+        elif choice==5:
+            saveInFile(emailDict)
+            print("Information saved")
+            break;
+        else:
+            print("invalid input")
 
-    #get two numbers 
-    num1 = random.randint(1,rangeValue)
-    num2 = random.randint(1,rangeValue)
-    
-    print("\nHow much is ", num1, "/" , num2)
-    #ask user to enter answer
-    result = input("Enter your answer (-1 to exit): ")
 
-    res = str(num1/num2)
-    
-    if(result == "-1"):
-        return -1
-    if(result == res):
-        correct+=1
-        print(goodResponse[random.randint(0,len(goodResponse)-1)])
-    else:
-        wrong+=1
-        print(errorResponse[random.randint(0,len(errorResponse)-1)])
-    return 0
-
-def options(option):
-    exitValue = 0
-    if(option == 1):
-        exitValue = addition()
-    elif(option == 2):
-        exitValue = subtraction()
-    elif(option == 3):
-        exitValue = multiplication()
-    elif(option == 4):
-        exitValue = division()
-    else :
-        option = random.randint(1,4) #for random operation
-        options(option)
-    return exitValue
-
-# function for operation Menu
-def operationMenu():
-    print("\n1 : Addition")
-    print("2 : Subtraction ")
-    print("3 : Multiplication ")
-    print("4 : Division ")
-    print("5 : Random operation")
-    
-    # getting operation type from user
-    option = int(input("Enter the operation (1 to 5):"))
-    exitValue = options(option)
-    if(exitValue == -1):
-        return
-    operationMenu()
-    
-#ask user to enter the difficulty level
-difficulty = input("Enter difficulty Level(1 or 2) : ")
-operationMenu()
-print("Number of correct answers :",correct)
-print("Number of wrong answers :",wrong)
-print("Thanks for playing!")
-
+main()
 
 
 
